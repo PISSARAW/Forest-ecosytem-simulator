4c4632ff8e0ad470cb6fa96189195c622639d72e
Ginks <pissabruney@hotmail.com>
2025-07-02 15:00:27 +0200

    Vector3D and Home

--- src/core/vector3-d.ts
+++ src/core/vector3-d.ts
@@ -3,39 +3,40 @@ export class Vector3D {
     
     // Méthodes de calcul vectoriel
     add(other: Vector3D): Vector3D{
-        return other;
+        return new Vector3D(this.x+other.x, this.y+other.y, this.z+other.z);
     }
     subtract(other: Vector3D): Vector3D{
-        return other;
+        return new Vector3D(this.x-other.x, this.y-other.y, this.z-other.z);
     }
     multiply(scalar: number): Vector3D{
-        return this;
+        return new Vector3D(scalar*this.x, scalar*this.y, scalar*this.z);
     }
     divide(scalar: number): Vector3D{
-        return this;
+        return new Vector3D(this.x/scalar, this.y/scalar, this.z/scalar);
     }
     dot(other: Vector3D): number{
-        return 0;
+        return this.x*other.x+this.y*other.y+this.z*other.z;
     }
     cross(other: Vector3D): Vector3D{
-        return other;
+        return new Vector3D(this.y*other.z-this.z*other.y, this.z*other.x-this.x*other.z, this.x*other.y-this.y*other.x);
     }
     magnitude(): number{
-        return 0;
+        return Math.pow(this.x, 2)+Math.pow(this.y, 2)+Math.pow(this.z,2);
     }
     normalize(): Vector3D{
-        return this;
+        let magnitude = this.magnitude();
+        return new Vector3D(this.x/magnitude, this.y/magnitude, this.z/magnitude);
     }
     distanceTo(other: Vector3D): number{
-        return other.x;
+        return Math.pow(this.x-other.x, 2) + Math.pow(this.y-other.y, 2) + Math.pow(this.z-other.z, 2) ;
     }
     clone(): Vector3D{
         return this;
     }
     equals(other: Vector3D): boolean{
-        return false;
+        return other===this;
     }
     toString(): string{
-        return "other";
+        return `(${this.x}, ${this.y}, ${this.z})`;
     }
 }
